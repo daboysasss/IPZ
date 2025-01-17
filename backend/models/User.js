d8e5bd211e7b8086ca7b1e19c92beac0f4ce24cc
Arsenii Bocharnikov <ba53727>
2025-01-17 15:02:05 +0100

    update

---
+++ backend/models/User.js
@@ -0,0 +1,13 @@
+const mongoose = require('mongoose');
+
+const UserSchema = new mongoose.Schema({
+  email: { type: String, required: true, unique: true },
+  name: { type: String, required: true },
+  password: { type: String, required: true },
+  role: { type: String, enum: ['student', 'instructor', 'admin'], required: true },
+  createdAt: { type: Date, default: Date.now },
+  ratings: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Rating' }],
+  schedule: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Schedule' }],
+});
+
+module.exports = mongoose.model('User', UserSchema);

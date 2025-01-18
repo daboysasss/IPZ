4aa9223d630bf9b1dae1a979c684bf5d1366445f
Arsenii Bocharnikov <ba53727>
2025-01-18 13:05:08 +0100

    update

--- backend/config/index.js
+++
@@ -1,5 +0,0 @@
-module.exports = {
-    secret: 'aboba', // Секрет для JWT
-    mongoURI: 'mongodb+srv://noname52:oralcumshot@cluster0.zohi8.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0', // Локальная база MongoDB
-};
-  

69a906615c49e7b127a113825855cc296ae08445
aayyllinnn <sa52087@zut.edu.pl>
2025-01-19 18:18:25 +0100

    Update theory.js

--- front/public/teoria/theory.js
+++ front/public/teoria/theory.js
@@ -11,5 +11,26 @@ document.addEventListener('DOMContentLoaded', () => {
             progressCircle.textContent = `${result}%`; // Обновляем текст прогресса
             progressCircle.style.color = parseInt(result) > 0 ? 'green' : 'red'; // Если результат больше 0, ставим зеленый цвет
         }
+
+        // Реализация смены темы
+        const themeToggle = document.getElementById('themeToggle');
+        const body = document.body;
+
+        // Устанавливаем начальную тему из localStorage
+        const savedTheme = localStorage.getItem('theme') || 'light-theme';
+        body.className = savedTheme;
+        themeToggle.textContent = savedTheme === 'dark-theme' ? '🌙' : '☀️';
+
+        // Слушатель для смены темы
+        themeToggle.addEventListener('click', () => {
+            body.classList.toggle('dark-theme');
+            body.classList.toggle('light-theme');
+
+            themeToggle.textContent = body.classList.contains('dark-theme') ? '🌙' : '☀️';
+
+            // Сохраняем текущую тему в localStorage
+            const currentTheme = body.classList.contains('dark-theme') ? 'dark-theme' : 'light-theme';
+            localStorage.setItem('theme', currentTheme);
+        });
     });
 });
